3a39023e137f28bab06e2c63eba34a618bbac4d1
jahangiralamdiu <jahangiralamdiu@gmail.com>
2015-02-23 21:02:00 +0600

    Add tiles integration and basic aop

---
+++ src/main/java/jh/springbasicapp/aspect/TracingAspect.java
@@ -0,0 +1,7 @@
+package jh.springbasicapp.aspect;
+
+/**
+ * Created by Jahangir on 23/02/2015.
+ */
+public class TracingAspect {
+}
---
+++ src/main/java/jh/springbasicapp/controller/BaseController.java
@@ -0,0 +1,7 @@
+package jh.springbasicapp.controller;
+
+/**
+ * Created by Jahangir on 23/02/2015.
+ */
+public class BaseController {
+}
